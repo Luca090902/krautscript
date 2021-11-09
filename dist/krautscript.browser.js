60bfc0664be6d24bd5fe2fcc1c0881b9e1882c4a
Luca <34965269+Luca090902@users.noreply.github.com>
2021-11-09 08:20:42 +0100

    Update krautscript.browser.js

--- dist/krautscript.browser.js
+++ dist/krautscript.browser.js
@@ -1,4 +1,4 @@
-FikaScript.load = function(url, callback, options) {
+KrautScript.load = function(url, callback, options) {
   var xhr;
   if (options == null) {
     options = {};
@@ -13,7 +13,7 @@ FikaScript.load = function(url, callback, options) {
     var _ref;
     if (xhr.readyState === 4) {
       if ((_ref = xhr.status) === 0 || _ref === 200) {
-        FikaScript.run(xhr.responseText, options);
+        KrautScript.run(xhr.responseText, options);
       } else {
         throw new Error("Could not load " + url);
       }
@@ -25,7 +25,7 @@ FikaScript.load = function(url, callback, options) {
   return xhr.send(null);
 };
 
-FikaScript.run = function(code, options) {
+KrautScript.run = function(code, options) {
   if (options == null) {
     options = {};
   }
@@ -40,38 +40,38 @@ if (typeof window === "undefined" || window === null) {
 }
 
 compile = function(code, options) {
-  return FikaScript.swedishToEnglish(code);
+  return KrautScript.germanToEnglish(code);
 }
 
 var runScripts,
   __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 runScripts = function() {
-  var fikas, fikatypes, execute, index, length, s, scripts;
+  var krauts, krauttypes, execute, index, length, s, scripts;
   scripts = window.document.getElementsByTagName('script');
-  fikatypes = ['text/fikascript'];
-  fikas = (function() {
+  krauttypes = ['text/krautscript'];
+  krauts = (function() {
     var _i, _len, _ref, _results;
     _results = [];
     for (_i = 0, _len = scripts.length; _i < _len; _i++) {
       s = scripts[_i];
-      if (_ref = s.type, __indexOf.call(fikatypes, _ref) >= 0) {
+      if (_ref = s.type, __indexOf.call(krauttypes, _ref) >= 0) {
         _results.push(s);
       }
     }
     return _results;
   })();
   index = 0;
-  length = fikas.length;
+  length = krauts.length;
   (execute = function() {
     var mediatype, options, script;
-    script = fikas[index++];
+    script = krauts[index++];
     mediatype = script != null ? script.type : void 0;
-    if (__indexOf.call(fikatypes, mediatype) >= 0) {
+    if (__indexOf.call(krauttypes, mediatype) >= 0) {
       if (script.src) {
-        return FikaScript.load(script.src, execute, options);
+        return KrautScript.load(script.src, execute, options);
       } else {
-        FikaScript.run(script.innerHTML, options);
+        KrautScript.run(script.innerHTML, options);
         return execute();
       }
     }
